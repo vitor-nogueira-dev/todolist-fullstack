5111258f087da25577a51db0c4ee1f640fc338fa
Vitor Nogueira <vslnogueira96@gmail.com>
2023-04-07 16:33:33 -0300

    feat: criando rotas e endpoints

---
+++ backend/src/router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const tasksController = require('./controllers/tasksController');
+const tasksMiddleware = require('./middlewares/tasksMiddleware');
+
+const router = express.Router();
+
+router.get('/tasks', tasksController.getAll);
+router.post(
+  '/tasks',
+  tasksMiddleware.validateFieldTitle,
+  tasksController.createdTask
+);
+router.delete('/tasks/:id', tasksController.deleteTask);
+router.put(
+  '/tasks/:id',
+  tasksMiddleware.validateFieldTitle,
+  tasksMiddleware.validateFieldStatus,
+  tasksController.updateTask
+);
+
+module.exports = router;
